162c5cc3466034d49ffcfbc61910827f76c37210
sanket <telisanket2002@gmail.com>
2023-06-11 23:59:56 +0530

    made navbar

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/home.css'
+import NavBar from './NavBar'
+const Home = () => {
+  return (
+    <div>
+    <NavBar></NavBar>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import "../styles/navbar.css"
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+        <div className='logo'>
+           logo
+        </div>
+
+        <div >
+            <ul className='options'>
+                <li>
+                    Contactme
+                </li>
+                <li>
+                    Home
+                </li>
+                <li>
+                     Projects
+                </li>
+                <li>
+                      Blogs
+                </li>
+
+            </ul>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default NavBar
